45008600e7c1a8108bb1f404f29c7e5d574bb1c7
Namita Shah <namitas@sfu.ca>
2017-12-01 00:45:31 -0800

    Random Forest

--- RandomForest/random_forest.py
+++ RandomForest/random_forest.py
@@ -1,5 +1,8 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import make_classification
+from sklearn.metrics import accuracy_score, confusion_matrix, roc_curve, auc
+from sklearn.model_selection import cross_val_score
+import matplotlib.pyplot as plt
 
 import pandas as pd
 import numpy as np
@@ -47,30 +50,72 @@ attributes.remove('HPSA_Ind')
 
 df = shuffle(df)
 
-train, test = train_test_split(df, test_size=0.3)
+# For train test split
+HPSA_input = df.drop('HPSA_Ind', axis=1)
+HPSA_target = df.HPSA_Ind
 
-X_train = np.array(train[attributes])
-X_test = np.array(test[attributes])
-
-Y_train = np.array(train[target_attribute])
-Y_test = np.array(test[target_attribute])
-
-# TODO: Take Train and Test Data anf fit into model
+X_train, X_test, Y_train, Y_test = train_test_split(HPSA_input, HPSA_target, test_size=0.3, random_state=24, stratify=HPSA_target)
 
 model = RandomForestClassifier(max_depth=20, random_state=0)
 model.fit(X_train, Y_train)
 
-# RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini',
-#             max_depth=2, max_features='auto', max_leaf_nodes=None,
-#             min_impurity_decrease=0.0, min_impurity_split=None,
-#             min_samples_leaf=1, min_samples_split=2,
-#             min_weight_fraction_leaf=0.0, n_estimators=10, n_jobs=1,
-#             oob_score=False, random_state=0, verbose=0, warm_start=False)
-
-# print(clf.feature_importances_)
-# [ 0.17287856  0.80608704  0.01884792  0.00218648]
-# print(clf.predict([[0, 0, 0, 0]]))
-
 score = model.score(X_test, Y_test)
-
 print(score)
+
+y_pred = model.predict(X_test)
+
+confusion = confusion_matrix(Y_test, y_pred)
+TP = confusion[1, 1]
+TN = confusion[0, 0]
+FP = confusion[0, 1]
+FN = confusion[1, 0]
+
+#Classification Accuracy
+classification_accuracy = (TP + TN) / float(TP + TN + FP + FN)*100 #accuracy_score(Y, y_pred)*100
+print("classification_accuracy", classification_accuracy)
+
+#Misclassification Rate/Classification error
+classification_error = (FP + FN) / float(TP + TN + FP + FN)*100
+
+#Sensitivity/True Positive Rate/Recall
+sensitivity = TP / float(FN + TP)*100 #or recall_score
+
+#Specificity
+specificity = TN / float(TN + FP)*100
+
+#False Positive Rate: When the actual value is negative, how often is the prediction incorrect?
+false_positive_rate = FP / float(TN + FP)*100
+
+#False Negative Rate: When the actual value is positive, how often is the prediction incorrect?
+false_negative_rate = FN / float(FN + TP) * 100
+
+#Precision
+precision = TP / float(TP + FP)*100
+
+#aoc, roc
+#store the predicted probabilities for class 1
+y_score = model.predict_proba(X_test)[:, 1]
+fpr, tpr, thresholds = roc_curve(Y_test, y_score)
+area_under_curve = auc(fpr, tpr) * 100
+
+print("------------------Metrics------------------")
+print("Random Forest-----------------------------------")
+print("Classification Accuracy = ", classification_accuracy)
+print("Classification Error = ", classification_error)
+print("False Positive Rate = ", false_positive_rate)
+print("False Negative Rate = ", false_negative_rate)
+print("True Positive Rate/Sensitivity/Recall = ", sensitivity)
+print("Precision = ", precision)
+print("Specificity = ", specificity)
+print("Area under curve = ", area_under_curve)
+
+print("\n\n\n-------------------Plot-------------------")
+plt.plot(fpr, tpr)
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.0])
+plt.rcParams['font.size'] = 12
+plt.title('ROC curve for HPSA classifier')
+plt.xlabel('False Positive Rate (1 - Specificity)')
+plt.ylabel('True Positive Rate (Sensitivity)')
+plt.grid(True)
+plt.show()
