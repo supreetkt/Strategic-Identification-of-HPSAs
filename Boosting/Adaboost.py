299063e9b661b1869c4d4a30b28a30580866b49e
Supreet Kaur Takkar <stakkar@sfu.ca>
2018-08-16 14:06:17 -0700

    Update Adaboost.py
    updated some comments

--- Boosting/Adaboost.py
+++ Boosting/Adaboost.py
@@ -9,7 +9,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, auc, roc_curve
 
 
-#1=No, 2=Yes in the dataset
+# 1=No, 2=Yes in the dataset
 def convert_target_label(x):
     if x == 1:
         return 0
@@ -21,7 +21,7 @@ df = pd.read_csv(
     r'C:\Users\Guest123\Desktop\726 - Machine Learning\Project\ML-HPSA-Pandas\Preprocessing\master_final.csv')
 col_names = df.columns
 
-#delete Nitrogen_Dioxide_Ind, Sulfur_Dioxide_Ind because they only have values 1s and don't affect the outcomes : 40 columns
+# delete Nitrogen_Dioxide_Ind, Sulfur_Dioxide_Ind because they only have values 1s and don't affect the outcomes : 40 columns
 del df['Nitrogen_Dioxide_Ind']
 del df['Sulfur_Dioxide_Ind']
 
@@ -33,7 +33,7 @@ exclusion_list = list(df.CHSI_State_Abbr.unique()) + \
                   ['Community_Health_Center_Ind']
 del df['CHSI_State_Abbr']
 
-#shuffling dataset : 90 columns
+# shuffling dataset : 90 columns
 df = shuffle(df)
 
 # selecting target label, converting columns from 1=No, 2=Yes to 0/1
@@ -42,7 +42,7 @@ HPSA_Ind_df = HPSA_Ind_df.apply(convert_target_label)
 df.Community_Health_Center_Ind = df.Community_Health_Center_Ind.apply(convert_target_label)
 del df['HPSA_Ind'] #89 columns
 
-#standardization: resultant = 89 columns
+# standardization: resultant = 89 columns
 for i in df.columns:
     mean = np.mean(df[i])
     std = np.std(df[i])
@@ -54,15 +54,15 @@ for i in df.columns:
 
 df = pd.concat([df, HPSA_Ind_df], axis=1) #90 columns
 
-#define datasets and class labels X, Y
+# define datasets and class labels X, Y
 array = df.values
 X = array[:,0:89]
 Y = array[:,89]
 
-#split into train and test
+# split into train and test
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=42, stratify = Y)
 
-#apply algorithm - 1. Adaboost
+# apply algorithm - 1. Adaboost
 num_trees = 40
 seed = 7
 
@@ -88,29 +88,29 @@ TN = confusion[0, 0]
 FP = confusion[0, 1]
 FN = confusion[1, 0]
 
-#Classification Accuracy
+# Classification Accuracy
 classification_accuracy = (TP + TN) / float(TP + TN + FP + FN)*100 #accuracy_score(Y, y_pred)*100
 
-#Misclassification Rate/Classification error
+# Misclassification Rate/Classification error
 classification_error = (FP + FN) / float(TP + TN + FP + FN)*100
 
-#Sensitivity/True Positive Rate/Recall
+# Sensitivity/True Positive Rate/Recall
 sensitivity = TP / float(FN + TP)*100 #or recall_score
 
-#Specificity/True Negative Rate
+# Specificity/True Negative Rate
 specificity = TN / float(TN + FP)*100
 
-#False Positive Rate: When the actual value is negative, how often is the prediction incorrect?
+# False Positive Rate: When the actual value is negative, how often is the prediction incorrect?
 false_positive_rate = FP / float(TN + FP)*100
 
-#False Negative Rate: When the actual value is positive, how often is the prediction incorrect?
+# False Negative Rate: When the actual value is positive, how often is the prediction incorrect?
 false_negative_rate = FN / float(FN + TP)*100
 
-#Precision
+# Precision
 precision = TP / float(TP + FP)*100
 
-#aoc, roc
-#store the predicted probabilities for class 1
+# aoc, roc
+# store the predicted probabilities for class 1
 y_score = adaboost.predict_proba(X_test)[:, 1]
 fpr, tpr, thresholds = roc_curve(Y_test, y_score)
 area_under_curve = auc(fpr, tpr) * 100
@@ -131,7 +131,7 @@ print('Precision = %.2f %%' % precision)
 print('Specificity = %.2f %%' % specificity)
 print('Area under curve = %.2f %%' % area_under_curve)
 
-#plot
+# plot
 plt.plot(fpr, tpr)
 plt.xlim([0.0, 1.0])
 plt.ylim([0.0, 1.0])
